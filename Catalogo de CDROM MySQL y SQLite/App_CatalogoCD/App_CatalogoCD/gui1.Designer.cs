ffcceada6961ff4523ea5fe2cc4091a77ba4911c
Liquid69 <er-salvi-69@hotmail.com>
2015-05-14 14:04:55 +0200

    diseño hecho

---
+++ Catalogo de CDROM MySQL y SQLite/App_CatalogoCD/App_CatalogoCD/gui1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace App_CatalogoCD
+{
+    partial class gui1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_leertodo = new System.Windows.Forms.Button();
+            this.btn_leerxml = new System.Windows.Forms.Button();
+            this.btn_añadiralazar = new System.Windows.Forms.Button();
+            this.btn_eliminarundvd = new System.Windows.Forms.Button();
+            this.btn_modificardvd = new System.Windows.Forms.Button();
+            this.btn_volcardvd = new System.Windows.Forms.Button();
+            this.btn_listardvdporpais = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(10, 8);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(421, 381);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btn_leertodo
+            // 
+            this.btn_leertodo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn_leertodo.Location = new System.Drawing.Point(437, 12);
+            this.btn_leertodo.Name = "btn_leertodo";
+            this.btn_leertodo.Size = new System.Drawing.Size(237, 23);
+            this.btn_leertodo.TabIndex = 1;
+            this.btn_leertodo.Text = "Leer todo los DVD";
+            this.btn_leertodo.UseVisualStyleBackColor = false;
+            // 
+            // btn_leerxml
+            // 
+            this.btn_leerxml.BackColor = System.Drawing.Color.LightGray;
+            this.btn_leerxml.Location = new System.Drawing.Point(437, 41);
+            this.btn_leerxml.Name = "btn_leerxml";
+            this.btn_leerxml.Size = new System.Drawing.Size(237, 23);
+            this.btn_leerxml.TabIndex = 2;
+            this.btn_leerxml.Text = "Leer todo los DVD en xml";
+            this.btn_leerxml.UseVisualStyleBackColor = false;
+            // 
+            // btn_añadiralazar
+            // 
+            this.btn_añadiralazar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn_añadiralazar.Location = new System.Drawing.Point(437, 70);
+            this.btn_añadiralazar.Name = "btn_añadiralazar";
+            this.btn_añadiralazar.Size = new System.Drawing.Size(237, 23);
+            this.btn_añadiralazar.TabIndex = 3;
+            this.btn_añadiralazar.Text = "Añadir un DVD al alzar";
+            this.btn_añadiralazar.UseVisualStyleBackColor = false;
+            // 
+            // btn_eliminarundvd
+            // 
+            this.btn_eliminarundvd.BackColor = System.Drawing.Color.LightGray;
+            this.btn_eliminarundvd.Location = new System.Drawing.Point(437, 99);
+            this.btn_eliminarundvd.Name = "btn_eliminarundvd";
+            this.btn_eliminarundvd.Size = new System.Drawing.Size(237, 23);
+            this.btn_eliminarundvd.TabIndex = 4;
+            this.btn_eliminarundvd.Text = "Eliminar un DVD";
+            this.btn_eliminarundvd.UseVisualStyleBackColor = false;
+            // 
+            // btn_modificardvd
+            // 
+            this.btn_modificardvd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn_modificardvd.Location = new System.Drawing.Point(437, 128);
+            this.btn_modificardvd.Name = "btn_modificardvd";
+            this.btn_modificardvd.Size = new System.Drawing.Size(237, 23);
+            this.btn_modificardvd.TabIndex = 5;
+            this.btn_modificardvd.Text = "Modificar un DVD";
+            this.btn_modificardvd.UseVisualStyleBackColor = false;
+            // 
+            // btn_volcardvd
+            // 
+            this.btn_volcardvd.BackColor = System.Drawing.Color.LightGray;
+            this.btn_volcardvd.Location = new System.Drawing.Point(437, 157);
+            this.btn_volcardvd.Name = "btn_volcardvd";
+            this.btn_volcardvd.Size = new System.Drawing.Size(237, 23);
+            this.btn_volcardvd.TabIndex = 6;
+            this.btn_volcardvd.Text = "Volcar un DVD a un fichero";
+            this.btn_volcardvd.UseVisualStyleBackColor = false;
+            // 
+            // btn_listardvdporpais
+            // 
+            this.btn_listardvdporpais.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn_listardvdporpais.Location = new System.Drawing.Point(437, 186);
+            this.btn_listardvdporpais.Name = "btn_listardvdporpais";
+            this.btn_listardvdporpais.Size = new System.Drawing.Size(237, 23);
+            this.btn_listardvdporpais.TabIndex = 7;
+            this.btn_listardvdporpais.Text = "Listar DVD por pais";
+            this.btn_listardvdporpais.UseVisualStyleBackColor = false;
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.BackColor = System.Drawing.Color.PeachPuff;
+            this.btn_salir.Location = new System.Drawing.Point(437, 366);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(237, 23);
+            this.btn_salir.TabIndex = 8;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = false;
+            // 
+            // gui1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 396);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_listardvdporpais);
+            this.Controls.Add(this.btn_volcardvd);
+            this.Controls.Add(this.btn_modificardvd);
+            this.Controls.Add(this.btn_eliminarundvd);
+            this.Controls.Add(this.btn_añadiralazar);
+            this.Controls.Add(this.btn_leerxml);
+            this.Controls.Add(this.btn_leertodo);
+            this.Controls.Add(this.listBox1);
+            this.Name = "gui1";
+            this.Text = "gui1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_leertodo;
+        private System.Windows.Forms.Button btn_leerxml;
+        private System.Windows.Forms.Button btn_añadiralazar;
+        private System.Windows.Forms.Button btn_eliminarundvd;
+        private System.Windows.Forms.Button btn_modificardvd;
+        private System.Windows.Forms.Button btn_volcardvd;
+        private System.Windows.Forms.Button btn_listardvdporpais;
+        private System.Windows.Forms.Button btn_salir;
+    }
+}
